c064e4e34345b2e2769f0b35e41b489b94a5395a
AngelGarcesV <angellgarces1024@gmail.com>
2024-04-18 23:22:07 -0500

    front ;)

--- Carrito/urls.py
+++ Carrito/urls.py
@@ -26,10 +26,6 @@ urlpatterns = [
     path('', RedirectView.as_view(url='login/', permanent=False), name='index'),
     path('admin/', admin.site.urls),
     path('tienda/', tienda, name="Tienda"),
-<<<<<<< HEAD
-    path('listaProductos/', lista_productos, name="listaProductos"),
-=======
->>>>>>> 55311896df52b0af62871fd0453b4125bc874aaf
     path('agregar/<int:producto_id>/', agregar_producto, name="Add"),
     path('eliminar/<int:producto_id>/', eliminar_producto, name="Del"),
     path('restar/<int:producto_id>/', restar_producto, name="Sub"),
